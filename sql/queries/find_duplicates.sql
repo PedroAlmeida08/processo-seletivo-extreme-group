c506b7786b1eb01066d52771704c97e57cb3b0ac
PedroAlmeida08 <jp_almeida@id.uff.br>
2025-06-07 16:02:16 -0300

    fix: changing column name

--- sql/queries/find_duplicates.sql
+++ sql/queries/find_duplicates.sql
@@ -1,5 +1,5 @@
 /* Encontra duplicatas na tabela PACIENTE do schema stg_prontuario, caso existam */
-SELECT cpf, COUNT(*) AS quantidade_duplicatas
+SELECT cpf, COUNT(*) AS quantidade
 FROM stg_prontuario.PACIENTE
 WHERE cpf IS NOT NULL
 GROUP BY cpf
